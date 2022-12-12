2d7c1acc7ae3140c0369acd9001af0b87ce3cb3e
skazzp <skazzp@gmail.com>
2022-12-12 23:52:57 +0200

    structure

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 
-const { STATIC_FILES_DIR } = require('./middlewares/avatarMiddleware');
-const contactsRouter = require('./routes/api/contactsRouter');
-const authRouter = require('./routes/api/authRouter');
+// const { STATIC_FILES_DIR } = require('./middlewares/avatarMiddleware');
+// const contactsRouter = require('./routes/api/contactsRouter');
+// const authRouter = require('./routes/api/authRouter');
 const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
@@ -13,11 +13,11 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/avatars', express.static(STATIC_FILES_DIR));
+// app.use('/avatars', express.static(STATIC_FILES_DIR));
 
-app.use('/api/contacts', contactsRouter);
+// app.use('/api/contacts', contactsRouter);
 
-app.use('/api/users', authRouter);
+// app.use('/api/users', authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
