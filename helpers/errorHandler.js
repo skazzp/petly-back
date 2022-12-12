4cfe37cf3fc16178fd0497a3cf29ac2933ff92b0
skazzp <skazzp@gmail.com>
2022-12-12 23:03:07 +0200

    src

---
+++ helpers/errorHandler.js
@@ -0,0 +1,12 @@
+function tryCatchWrapper(request) {
+  return async (req, res, next) => {
+    try {
+      await request(req, res, next);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  };
+}
+
+module.exports = { tryCatchWrapper };
---
+++ server.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const app = require('./app');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.MONGO_DB_URI;
+mongoose.Promise = global.Promise;
+
+const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connection
+  .then(() => {
+    app.listen(PORT, function () {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch(err => console.log(`Server not running. Error message: ${err.message}`));
