14f69633ec6d6ddf72e6e30f066758440c18424c
ihumentsev <a.ihumentsev@gmail.com>
2022-12-14 16:38:31 +0100

    add-patternValidator

--- helpers/validations.js
+++ helpers/validations.js
@@ -1,8 +1,8 @@
 const Joi = require("joi");
-
+const pattern = "^[a-zA-Z0-9]{3,30}$";
 const schemaRegister = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  password: Joi.string().pattern(new RegExp(pattern)),
   phone: Joi.string().required(),
   city: Joi.string().required(),
   name: Joi.string().min(3).max(15).required(),
@@ -10,7 +10,7 @@ const schemaRegister = Joi.object({
 
 const schemaLogin = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  password: Joi.string().pattern(new RegExp(pattern)),
 });
 
 module.exports = {
