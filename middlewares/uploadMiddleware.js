f50ef91a95d3aa7ebc75dd1c6ad67641cfc36927
skazzp <skazzp@gmail.com>
2022-12-18 23:52:02 +0200

    test

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,31 +1,27 @@
-const fs = require("fs/promises");
-const { cloudUpload } = require("../service/modules/cloudinaryService");
+const fs = require('fs/promises');
+const { cloudUpload } = require('../service/modules/cloudinaryService');
 
 const uploadMiddleware = async (req, res, next) => {
   if (!req.file) {
     req.body.photoURL =
-      "https://res.cloudinary.com/dnkfxtdl2/image/upload/v1671266714/Catdog_uhpgig.jpg";
-    req.body.photoId = "";
+      'https://res.cloudinary.com/dnkfxtdl2/image/upload/v1671266714/Catdog_uhpgig.jpg';
+    req.body.photoId = '';
     return next();
   }
 
   const { path: tempUpload, originalname } = req.file;
-  console.log(originalname);
-  const format = originalname.split(".").pop();
+  console.log(originalname, req.baseUrl);
+  const format = originalname.split('.').pop();
   const { userId } = req;
 
-  const folder = req.baseUrl.split("/")[3];
-  console.log("folder", folder);
+  // const folder = req.baseUrl.split('/')[3];
+  const folder = 'users';
+  console.log('folder', folder);
 
-  const id = `${userId + "_" + Date.now()}`;
+  const id = `${userId + '_' + Date.now()}`;
 
   try {
-    const { resultUrl, resultId } = await cloudUpload(
-      tempUpload,
-      id,
-      folder,
-      format
-    );
+    const { resultUrl, resultId } = await cloudUpload(tempUpload, id, folder, format);
 
     req.body.photoURL = resultUrl;
     req.body.photoId = resultId;
--- service/modules/currentUser.js
+++ service/modules/currentUser.js
@@ -1,15 +1,16 @@
-const { Users } = require("../schemas/Users");
+const { Users } = require('../schemas/Users');
 
-const getInfo = async (req) => {
+const getInfo = async req => {
   const user = await Users.findOne({ _id: req.userId });
   return user;
 };
 
-const updateInfo = (req) => {
-  let birthday = "";
-  if (req.body.birthday !== "") {
-    birthday = new Date(req.body.birthday).toISOString().split("T")[0];
+const updateInfo = req => {
+  let birthday = '';
+  if (req.body.birthday && req.body.birthday !== '') {
+    birthday = new Date(req.body.birthday).toISOString().split('T')[0];
   }
+  console.log(123, req.body.photoURL);
   const user = Users.findByIdAndUpdate(
     { _id: req.userId },
     {
@@ -17,7 +18,7 @@ const updateInfo = (req) => {
       name: req.body.name,
       phone: req.body.phone,
       city: req.body.city,
-      avatarURL: req.body.avatarURL,
+      avatarURL: req.body.photoURL,
       birthday: birthday,
     },
     { new: true }
