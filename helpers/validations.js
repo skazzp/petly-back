0b9a70aa58ea2934439a60523875ee6141886667
skazzp <skazzp@gmail.com>
2022-12-19 17:50:12 +0200

    fix validation

--- helpers/validations.js
+++ helpers/validations.js
@@ -1,5 +1,5 @@
-const Joi = require("joi");
-const pattern = "^[a-zA-Z0-9]{3,30}$";
+const Joi = require('joi');
+const pattern = '^[a-zA-Z0-9]{3,30}$';
 const schemaRegister = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().pattern(new RegExp(pattern)),
@@ -17,7 +17,7 @@ const schemaLogin = Joi.object({
 const schemaCreatePet = Joi.object({
   breed: Joi.string().required(),
   photoURL: Joi.string().required(),
-  photoId: Joi.string(),
+  photoId: Joi.string().allow(''),
   name: Joi.string().min(3).max(15).required(),
   comments: Joi.string(),
   birthday: Joi.string(),
