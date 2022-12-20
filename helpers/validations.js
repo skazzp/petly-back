c4c044092ece708dd7caf9feb875d298c777d64d
Dmytro Stupak <stupakbf@gmail.com>
2022-12-20 12:00:36 +0200

    small fix

--- helpers/validations.js
+++ helpers/validations.js
@@ -1,11 +1,11 @@
-const Joi = require('joi');
-const pattern = '^[a-zA-Z0-9]{3,30}$';
+const Joi = require("joi");
+const pattern = "^[a-zA-Z0-9]{3,30}$";
 const schemaRegister = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().pattern(new RegExp(pattern)),
   phone: Joi.string().required(),
   city: Joi.string().required(),
-  name: Joi.string().min(3).max(15).required(),
+  name: Joi.string().min(2).max(16).required(),
   birthday: Joi.string(),
 });
 
@@ -14,17 +14,26 @@ const schemaLogin = Joi.object({
   password: Joi.string().pattern(new RegExp(pattern)),
 });
 
+const schemaUserUpdate = Joi.object({
+  email: Joi.string().email(),
+  phone: Joi.string().required(),
+  city: Joi.string().required(),
+  name: Joi.string().min(2).max(16),
+  birthday: Joi.string(),
+});
+
 const schemaCreatePet = Joi.object({
-  breed: Joi.string().required(),
+  breed: Joi.string().min(2).max(16).required(),
   photoURL: Joi.string().required(),
-  photoId: Joi.string().allow(''),
-  name: Joi.string().min(3).max(15).required(),
-  comments: Joi.string(),
+  photoId: Joi.string().allow(""),
+  name: Joi.string().min(2).max(16).required(),
+  comments: Joi.string().min(8).max(120),
   birthday: Joi.string(),
 });
 
 module.exports = {
   schemaRegister,
   schemaLogin,
+  schemaUserUpdate,
   schemaCreatePet,
 };
