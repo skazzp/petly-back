3c1ae7ef26c44d335ea98713fdf7b5f62de507a2
seacat271 <seacat271083@gmail.com>
2022-12-26 17:37:55 +0200

    fix oauth

--- routes/api/googleAuth.js
+++ routes/api/googleAuth.js
@@ -1,31 +1,19 @@
-const express = require('express');
-const passport = require('passport');
-// const {
-//   // googleAuthenticate,
-//   // googleRedirectMiddelware,
-// } = require('../../middlewares/authGoogleMiddleware');
-require('../../helpers/oauthHelpers');
-// const errorHandler = require('../../helpers/errorHandler.js');
-const googleRedirect = require('../../controllers/google/googleRedirect');
+const express = require("express");
+const passport = require("passport");
+require("../../helpers/oauthHelpers");
+
+const googleRedirect = require("../../controllers/google/googleRedirect");
 const router = express.Router();
 
 router.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] })
-  // googleAuthenticate
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
 );
 
-// router.get("/google/callback", googleRedirectMiddelware, (req, res) => {
-//   const token = req.user.token;
-//   res.redirect(
-//     "http://localhost:3000/petly-front/google-redirect" + `?token=${token}`
-//   );
-// });
-
 router.get(
-  '/google/callback',
-  passport.authenticate('google', {
-    failureRedirect: 'http://localhost:3000/petly-front/signup',
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "http://localhost:3000/petly-front/signup",
   }),
   googleRedirect
 );
