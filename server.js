ef414a8dbfb3b9b6f5bbd87616a7283b201d13fe
FoX <you@example.com>
2023-02-26 01:46:38 +0200

    env

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const mongoose = require('mongoose');
-const app = require('./app');
+const mongoose = require("mongoose");
+const app = require("./app");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
+
 const uriDb = process.env.MONGO_DB_URI;
 mongoose.Promise = global.Promise;
 
 const connection = mongoose.connect(uriDb, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+ useNewUrlParser: true,
+ useUnifiedTopology: true,
 });
 
 connection
-  .then(() => {
-    app.listen(PORT, function () {
-      console.log(`Server running. Use our API on port: ${PORT}`);
-    });
-  })
-  .catch(err => console.log(`Server not running. Error message: ${err.message}`));
+ .then(() => {
+  app.listen(PORT, function () {
+   console.log(`Server running. Use our API on port: ${PORT}`);
+  });
+ })
+ .catch((err) =>
+  console.log(`Server not running. Error message: ${err.message}`)
+ );
